9b6f3d874ef27cb8aed39c0ebedd7cdd02a425af
juananra <juananra@gmail.com>
2023-01-25 17:11:44 +0100

    Test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from myMigrations import *
 if __name__ == '__main__':
     package_name = format_package_name(PROJECT_NAME)
     package_name_and_path = SAVE_PATH + '\\' + package_name
-    print(package_name_and_path)
+    #print(package_name_and_path)
 
     create_package(SOURCE_BASE_URL, SOURCE_USERNAME, SOURCE_PASSWORD, FOLDER_ID, package_name_and_path,
                    PACKAGE_DESCRIPTION, PROJECT_NAME)
